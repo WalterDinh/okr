bd249867a1b57994cb9c6fa6a28cf66410bcd23c
manhma <dvm2000hy@gmail.com>
2022-08-15 14:19:30 +0700

    createColorApp

---
+++ constants/colorApp.ts
@@ -0,0 +1,10 @@
+export const AppColor = {
+    backgroundColor: '#E5E5E5',
+    primaryColor: '#18202E',
+    whiteColor: '#FFFFFF',
+    blackColor: '#000000',
+    gulfBlue: '#35435B',
+    gray100: '#D1D2D5',
+    gray200: '#8C9097',
+    gray300: '#5E636D',
+}
