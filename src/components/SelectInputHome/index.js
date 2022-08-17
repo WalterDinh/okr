25cfa62d97eb3fe63c7ab8ec914de1628e36cf54
manhma <dvm2000hy@gmail.com>
2022-08-17 11:51:34 +0700

    updateInputSelect

--- src/components/SelectInputHome/index.js
+++ src/components/SelectInputHome/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function SelectInputHome(props) {
-  const { placeholder, listOption, width } = props;
+  const { placeholder, listOption, width, onChangeSelectInput } = props;
 
+  const handleChange = (e) => {
+    console.log('e.target.value: ', e.target.value);
+    onChangeSelectInput(e.target.value);
+  };
   return (
     <div className="select-input-home">
-      <select className="select-input" style={{ width: `${width}` }}>
+      <select className="select-input" style={{ width: `${width}` }} onChange={handleChange}>
         <option value="">{placeholder}</option>
         {listOption?.map((option) => (
           <option key={option.value} value={option.value}>
