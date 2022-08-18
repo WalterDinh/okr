dd738e60c765fd8825b0bdac13fa64a1de9ae9ce
thanhluan208 <skthunte33@gmail.com>
2022-08-18 15:09:38 +0700

    update header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -49,6 +49,7 @@ const Header = ({ onClick, nofication, name, onClickBack }) => {
     },
     'Button-4': {
       border: '0',
+      borderTop: 'solid 1px #C4C4C4',
       padding: '10px 0',
       background: 'transparent',
       width: '100%',
@@ -58,97 +59,99 @@ const Header = ({ onClick, nofication, name, onClickBack }) => {
 
   //! Render
   return (
-    <div
-      className="header2"
-      style={{
-        justifyContent: isBack ? 'space-between' : 'right',
-      }}
-    >
-      {isBack && (
-        <div className="left">
-          <Button
-            onClick={onClickBack}
-            innerText=""
-            icon={<BiArrowBack />}
-            type="secondary"
-            style={buttonStyle['Button-1']}
-          ></Button>
-        </div>
-      )}
-      <div className="right">
-        <div className="addBtn">
-          <Button
-            type="primary"
-            innerText={t('header:add')}
-            onClick={onClick}
-            icon={<HiPlus />}
-            borderRadius="round"
-            style={buttonStyle['Button-2']}
-          />
-        </div>
-        <div className="utilities">
-          <CommonIcons.Clock size={SIZE_ICON} />
-          <div className="bell">
-            {nofication && <div className="notification">{nofication}</div>}
-            <CommonIcons.Bell size={SIZE_ICON} />
+    <Fragment>
+      <div
+        className="header2"
+        style={{
+          justifyContent: isBack ? 'space-between' : 'right',
+        }}
+      >
+        {isBack && (
+          <div className="left">
+            <Button
+              onClick={onClickBack}
+              innerText=""
+              icon={<BiArrowBack />}
+              type="secondary"
+              style={buttonStyle['Button-1']}
+            ></Button>
           </div>
-        </div>
-        <div className="user">
-          <div className="avatar"></div>
-          <Button
-            innerText={name}
-            onClick={() => {
-              setDropdown(!dropDown);
-            }}
-            icon={
-              dropDown ? (
-                <AiOutlineDown
-                  style={{
-                    transform: 'rotate(180deg)',
-                    transition: 'all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-                  }}
-                />
-              ) : (
-                <AiOutlineDown
-                  style={{
-                    transition: 'all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
-                  }}
-                />
-              )
-            }
-            type="secondary"
-            style={buttonStyle['Button-3']}
-          ></Button>
-          <div className={dropDown ? 'userMenu' : 'userMenu-disable'}>
-            {dropDown ? (
-              <Fragment>
-                <div className="dropDown-btn">
-                  <Button
-                    type="primary"
-                    innerText={t('message:add')}
-                    onClick={onClick}
-                    icon={<HiPlus />}
-                    borderRadius="round"
-                    style={{ width: '100%' }}
+        )}
+        <div className="right">
+          <div className="addBtn">
+            <Button
+              type="primary"
+              innerText={t('messages:add')}
+              onClick={onClick}
+              icon={<HiPlus />}
+              borderRadius="round"
+              style={buttonStyle['Button-2']}
+            />
+          </div>
+          <div className="utilities">
+            <CommonIcons.Clock size={SIZE_ICON} />
+            <div className="bell">
+              {nofication && <div className="notification">{nofication}</div>}
+              <CommonIcons.Bell size={SIZE_ICON} />
+            </div>
+          </div>
+          <div className="user">
+            <div className="avatar"></div>
+            <Button
+              innerText={name}
+              onClick={() => {
+                setDropdown(!dropDown);
+              }}
+              icon={
+                dropDown ? (
+                  <AiOutlineDown
+                    style={{
+                      transform: 'rotate(180deg)',
+                      transition: 'all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+                    }}
                   />
-                </div>
-                {userMenu.map((item, index) => (
-                  <Button
-                    innerText={item.name}
-                    type="secondary"
-                    onClick={item.onClick}
-                    key={index}
-                    style={buttonStyle['Button-4']}
+                ) : (
+                  <AiOutlineDown
+                    style={{
+                      transition: 'all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+                    }}
                   />
-                ))}
-              </Fragment>
-            ) : (
-              ''
-            )}
+                )
+              }
+              type="secondary"
+              style={buttonStyle['Button-3']}
+            ></Button>
           </div>
         </div>
       </div>
-    </div>
+      <div className={dropDown ? 'userMenu' : 'userMenu-disable'}>
+        {dropDown ? (
+          <Fragment>
+            <div className="dropDown-btn">
+              <Button
+                type="primary"
+                innerText={t('messages:add')}
+                onClick={onClick}
+                icon={<HiPlus />}
+                borderRadius="round"
+                style={{ width: '100%' }}
+              />
+            </div>
+            {userMenu.map((item, index) => (
+              <Button
+                innerText={item.name}
+                type="secondary"
+                onClick={item.onClick}
+                key={index}
+                style={buttonStyle['Button-4']}
+              />
+            ))}
+          </Fragment>
+        ) : (
+          ''
+        )}
+      </div>
+    </Fragment>
   );
 };
 
