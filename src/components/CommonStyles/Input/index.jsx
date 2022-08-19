a8230b30ff069aa2937d43a84831c4702e2c4cab
AnhPham291297 <anh.potter1997@gmail.com>
2022-08-19 09:30:24 +0700

    fix feedback and input

--- src/components/CommonStyles/Input/index.jsx
+++ src/components/CommonStyles/Input/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Input = (props) => {
   const { field, form, label, icon, placeholder, style, type } = props;
   const { name } = field;
-  ``;
   const { errors, touched } = form;
 
   const isErrors = errors[name] && touched[name];
@@ -12,13 +11,9 @@ const Input = (props) => {
   return (
     <>
       {label && <div className="label">{label}</div>}
-      <div className="search-box">
-        {icon && <div className="icon">{props?.icon}</div>}
-        {icon ? (
-          <input className="input" style={props?.style} {...props} {...field} />
-        ) : (
-          <input className="input2" style={props?.style} {...props} {...field} />
-        )}
+      <div className="search-box" style={style}>
+        {icon && <div className="icon">{icon}</div>}
+        <input className={icon ? 'input' : 'input2'} type={type} placeholder={placeholder} {...field} />
       </div>
       {isErrors && <div className="error">{errorMessage}</div>}
     </>
