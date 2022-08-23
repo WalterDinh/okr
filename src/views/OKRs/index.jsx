9ab1dfd801a8a7c4ef8bd9013068b5bd4a54ba36
NguyenTienDat <tiendatnguyenit@gmail.com>
2022-08-23 16:12:24 +0700

    delete btn

--- src/views/OKRs/index.jsx
+++ src/views/OKRs/index.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
-import CommonStyles from 'components/CommonStyles';
-import CommonIcons from '../../components/icons';
-import { passwordRegex } from 'constants';
-import { useTranslation } from 'react-i18next';
 
 const OKRs = () => {
-  const { t } = useTranslation();
-  const history = useHistory();
   //! State
 
   //! Function
-  const ResetPasswordSchema = Yup.object().shape({
-    password: Yup.string()
-      .matches(passwordRegex, t('messages:wrong-format', { key: t('common:password') }))
-      .required(t('messages:required_field', { key: t('common:password') })),
-
-    reEnterPassword: Yup.string()
-      .matches(passwordRegex, t('messages:wrong-format', { key: t('common:password') }))
-      .required(t('messages:required_field', { key: t('common:re-enter-password') })),
-  });
-  const handleClick = () => {
-    history.push('/okrs/create-new-okrs');
-  };
 
   //! Render
   return (
     <div className="container-okr">
-      <CommonStyles.Button
-        color="primary"
-        borderRadius="round"
-        innerText={t('common:submit')}
-        onClick={handleClick}
-      />
+      
     </div>
   );
 };
